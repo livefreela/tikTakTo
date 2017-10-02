3e0c5625fa5d1fe15e433354b64cf4897bbe8787
livefreela <michaelbrawer@me.com>
2017-10-01 19:39:27 -0700

    added tile selector logic

--- JS/app.js
+++ JS/app.js
@@ -1,46 +1,98 @@
 /*----- constants -----*/
+
+
+
+/*----- app's state (variables) -----*/
 var inPlay = false;
 var win = false;
+var currentTurn = 'X';
+var xOwns = [];
+var oOwns = [];
 
-var r1c1 = document.getElementById('11');
-var r1c2 = document.getElementById('12');
-var r1c3 = document.getElementById('13');
-var r2c1 = document.getElementById('21');
-var r2c2 = document.getElementById('22');
-var r2c3 = document.getElementById('23');
-var r3c1 = document.getElementById('31');
-var r3c2 = document.getElementById('32');
-var r3c3 = document.getElementById('33');
-
-var currentTurn = 'x';
-/*----- app's state (variables) -----*/
-var table = document.querySelector('table');
-
-var resetButton = document.querySelector('.reset');
 /*----- cached element references -----*/
+
+var r1c1 = document.getElementById('r1c1');
+var r1c2 = document.getElementById('r1c2');
+var r1c3 = document.getElementById('r1c3');
+var r2c1 = document.getElementById('r2c1');
+var r2c2 = document.getElementById('r2c2');
+var r2c3 = document.getElementById('r2c3');
+var r3c1 = document.getElementById('r3c1');
+var r3c2 = document.getElementById('r3c2');
+var r3c3 = document.getElementById('r3c3');
+
+var tiles = document.querySelectorAll("div.container div div");
+var resetButton = document.querySelector('.reset');
+var display = document.getElementById('display');
 /*----- event listeners -----*/
-table.addEventListener('click', handleClick);
+
+tiles.forEach(function(i){
+  i.addEventListener("click", handleClick);
+});
+
 resetButton.addEventListener('click', function(){
   clearBoard();
 });
 
 /*----- functions -----*/
-function handleClick(evt){
-  evt.target.classList.add("blue");
 
-};
+
+
+function handleClick(evt){
+  if (currentTurn === "X"){
+    if (oOwns.indexOf(evt.target.id) !== -1){
+      return;
+    } else {
+      xOwns.push(evt.target.id);
+      evt.target.textContent = "O";
+    }
+  } else if(currentTurn === "O") {
+    if (xOwns.indexOf(evt.target.id) !== -1){
+      return; }else{
+        oOwns.push(evt.target.id);
+        evt.target.textContent = "O";
+      }
+    }
+    evt.target.classList.add(currentTurn);
+    console.log(evt.target.id);
+    winCheck();
+    // nextTurn();
+  }
+
+function nextTurn(){
+  currentTurn === "X" ? currentTurn = "O" : currentTurn = "X";
+  display.textContent = "Ready Player " + currentTurn;
+
+}
 
 function clearBoard() {
-  document.classList.remove("blue");
+  // allCells.forEach(function(i){
+  //   i.classList.add('blue');
+  // });
+  resetTiles();
 	// $('.tile').removeClass('played');
 	// $('.tile').removeClass('O-play');
 	// $('.tile').removeClass('X-play');
 	// $('.tile').html('');
 	// $('.tile').addClass('free');
-  alert("reset");
+
+  xOwns = [];
+  oOwns = [];
 
 }
 
-function nextTurn (){
-  currentTurn = "x" ? currentTurn = "o" : currentTurn = "x";
+function winCheck(){
+if (xOwns.indexOf("r1c1") === -1 && xOwns.indexOf("r1c2") === -1 && xOwns.indexOf("r1c3") === -1){
+  alert('win');
+} else {
+  nextTurn();
+}
+}
+
+function resetTiles(){
+  for (var i = 0; i < tiles.length; i++ ){
+    tiles[i].classList.remove('X');
+    tiles[i].classList.remove('O');
+
+  }
 }
